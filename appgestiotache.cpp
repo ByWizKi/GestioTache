5d70954e3f142bc0dbfc3f65c02e7d155a9b2bb6
ByWizKi <enzoth39260@gmail.com>
2023-03-21 11:59:40 +0100

    application texte terminer

--- appgestiotache.cpp
+++ appgestiotache.cpp
@@ -1,4 +1,9 @@
 #include "appgestiotache.h"
+<<<<<<< HEAD
+=======
+#include "QtCore/qobject.h"
+#include "ui_appgestiotache.h"
+>>>>>>> 0fa7646 (application texte terminer)
 
 AppGestioTache::AppGestioTache()
 {
